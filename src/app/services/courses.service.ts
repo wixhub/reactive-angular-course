fa4f7c07e11b386bf2fcef697cf0d270caf7298c
Jana Semenova <jana.semenova@yolawo.de>
2022-01-03 16:46:19 +0100

    added service

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Course, sortCoursesBySeqNo } from "../model/course";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CoursesService {
+  constructor(private http: HttpClient) {}
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http
+      .get<Course[]>("/api/courses")
+      .pipe(map((res) => res["payload"].sort(sortCoursesBySeqNo)));
+  }
+}
