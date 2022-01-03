1fdd64ee9f7589d70ffac9e51f4ae24d97c8a9bc
Jana Semenova <jana.semenova@yolawo.de>
2022-01-03 17:01:45 +0100

    added shareReplay

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 import { Course, sortCoursesBySeqNo } from "../model/course";
 
 @Injectable({
@@ -11,8 +11,9 @@ export class CoursesService {
   constructor(private http: HttpClient) {}
 
   loadAllCourses(): Observable<Course[]> {
-    return this.http
-      .get<Course[]>("/api/courses")
-      .pipe(map((res) => res["payload"].sort(sortCoursesBySeqNo)));
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((res) => res["payload"].sort(sortCoursesBySeqNo)),
+      shareReplay()
+    );
   }
 }
