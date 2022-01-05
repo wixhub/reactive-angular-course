de5c3529f8bda02053b764e488ae3600612258a1
Jana Semenova <jana.semenova@yolawo.de>
2022-01-05 08:17:45 +0100

    fix behavior

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, ReplaySubject } from "rxjs";
-import { map, shareReplay, tap } from "rxjs/operators";
+import { BehaviorSubject, Observable } from "rxjs";
 import { User } from "../model/user";
+import { map, shareReplay, tap } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
 
 const AUTH_DATA = "auth_data";
 
@@ -10,15 +10,20 @@ const AUTH_DATA = "auth_data";
   providedIn: "root",
 })
 export class AuthStore {
-  private subject = new ReplaySubject<User>(null);
+  private subject = new BehaviorSubject<User>(null);
+
   user$: Observable<User> = this.subject.asObservable();
+
   isLoggedIn$: Observable<boolean>;
   isLoggedOut$: Observable<boolean>;
 
   constructor(private http: HttpClient) {
     this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
-    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggIn) => !loggIn));
+
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+
     const user = localStorage.getItem(AUTH_DATA);
+
     if (user) {
       this.subject.next(JSON.parse(user));
     }
@@ -27,7 +32,7 @@ export class AuthStore {
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>("/api/login", { email, password }).pipe(
       tap((user) => {
-        (user) => this.subject.next(user);
+        this.subject.next(user);
         localStorage.setItem(AUTH_DATA, JSON.stringify(user));
       }),
       shareReplay()
