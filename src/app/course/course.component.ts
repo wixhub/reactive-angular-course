a63478322554c01fa42a87fac7b4378cf436a3b7
Jana Semenova <jana.semenova@yolawo.de>
2022-01-05 07:52:56 +0100

    Default Data Values

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -52,9 +52,13 @@ export class CourseComponent implements OnInit {
   ngOnInit() {
     const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
 
-    const course$ = this.coursesService.loadCourseById(courseId);
+    const course$ = this.coursesService
+      .loadCourseById(courseId)
+      .pipe(startWith(null));
 
-    const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    const lessons$ = this.coursesService
+      .loadAllCourseLessons(courseId)
+      .pipe(startWith([]));
 
     this.data$ = combineLatest([course$, lessons$]).pipe(
       map(([course, lessons]) => {
